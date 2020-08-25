3eebf120a6b7fdba60d63db734ad2252ce33da5d
Aseanseen <sean.lim.110@gmail.com>
2020-08-25 13:30:36 +0800

    Add echo and exit (Level-1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        String greeting = "―――――――――――――――――――――――――――――――――\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "―――――――――――――――――――――――――――――――――\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "―――――――――――――――――――――――――――――――――\n";
-        System.out.println(greeting);
+        Scanner commandObj = new Scanner(System.in);
+        printGreeting();
+
+        String command = commandObj.nextLine();
+        // Keeps taking and printing user output while input is not bye
+        while (!command.equals("bye")) {
+            printLine();
+            System.out.println(command);
+            printLine();
+            command = commandObj.nextLine();
+        }
+        printBye();
+    }
+    public static void printLine() {
+        System.out.println("―――――――――――――――――――――――――――――――――");
+    }
+    public static void printGreeting(){
+        printLine();
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        printLine();
+    }
+    public static void printBye(){
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
     }
 }
