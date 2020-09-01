1a89c295a8a2f73e0c2e12b1dd5eb52223be6fdd
Aseanseen <sean.lim.110@gmail.com>
2020-09-01 11:43:25 +0800

    Add instructions, exception message for done command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,12 +15,18 @@ public class Duke {
     }
     public static void printLine() {
         String dash = "\u2500";
-        System.out.println(dash.repeat(50));
+        System.out.println(dash.repeat(60));
     }
     public static void printGreeting(){
         printLine();
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
+        System.out.println(" - Add a todo e.g. todo read book");
+        System.out.println(" - Add a deadline e.g. deadline do quiz /by 1st Sept");
+        System.out.println(" - Add an event e.g. event prom /at Mon 6pm");
+        System.out.println(" - List all added tasks e.g. list");
+        System.out.println(" - Complete a task e.g. done 1");
+        System.out.println(" - Say bye to me e.g. bye");
         printLine();
     }
     public static void printBye(){
@@ -43,8 +49,13 @@ public class Duke {
             break;
         case "done":
             int listNum = Integer.parseInt(taskObj.next());
-            commands[listNum-1].markAsDone();
-            System.out.println(" Nice! I've marked this task as done:");
+            if(commands[listNum-1].isDone){
+                System.out.println(" This task has already been marked as done!");
+            }
+            else{
+                commands[listNum-1].markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+            }
             System.out.println("   " + commands[listNum-1].toString());
             break;
         case "todo":
@@ -52,6 +63,7 @@ public class Duke {
             commands[Task.totalNumOfTasks] = new ToDo(toDoTask);
             System.out.println(" Got it. I've added this task: " + System.lineSeparator() + "   " + commands[Task.totalNumOfTasks-1].toString());
             System.out.println(" Now you have " + Task.totalNumOfTasks + " tasks in the list.");
+            System.out.println(" Remember, you can enter \"list\" to view all tasks");
             break;
         case "deadline":
             int byIndex = command.indexOf("/by");
@@ -60,6 +72,7 @@ public class Duke {
             commands[Task.totalNumOfTasks] = new Deadline(deadlineDescription,deadlineBy);
             System.out.println(" Got it. I've added this task: " + System.lineSeparator() + "   " + commands[Task.totalNumOfTasks-1].toString());
             System.out.println(" Now you have " + Task.totalNumOfTasks + " tasks in the list.");
+            System.out.println(" Remember, you can enter \"list\" to view all tasks");
             break;
         case "event":
             int atIndex = command.indexOf("/at");
@@ -68,6 +81,7 @@ public class Duke {
             commands[Task.totalNumOfTasks] = new Event(eventDescription,eventAt);
             System.out.println(" Got it. I've added this task: " + System.lineSeparator() + "   " + commands[Task.totalNumOfTasks-1].toString());
             System.out.println(" Now you have " + Task.totalNumOfTasks + " tasks in the list.");
+            System.out.println(" Remember, you can enter \"list\" to view all tasks");
             break;
         default:
             System.out.println(" Invalid command!");
